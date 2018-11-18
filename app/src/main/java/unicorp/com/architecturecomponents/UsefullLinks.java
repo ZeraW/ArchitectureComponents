7fcae5691eaf0aa5c4fd74203f6bbd0aed0b3c90
ZeraW <hema_eie552@yahoo.com>
2018-11-18 16:27:56 +0200

    useful links

---
+++ app/src/main/java/unicorp/com/architecturecomponents/UsefullLinks.java
@@ -0,0 +1,17 @@
+package unicorp.com.architecturecomponents;
+
+/**
+ * Created by Hima on 11/18/2018.
+ */
+
+ class UsefullLinks {
+    // this class to share links
+    private String architectureComponents_Link,viewModel_Link,getContext_Link;
+
+    private void links(){
+        architectureComponents_Link = "https://developer.android.com/topic/libraries/architecture/";
+        viewModel_Link = "https://developer.android.com/topic/libraries/architecture/viewmodel#java";
+        getContext_Link = "https://stackoverflow.com/questions/30278290/what-is-the-difference-between-this-getcontext-and-getactivity";
+    }
+
+}
